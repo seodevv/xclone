204a925756e87b9aac645c2f407861ab6f7a4713
seodevv <seodev17@gmail.com>
2024-07-23 01:16:09 +0900

    Modify metadata in root layout

--- src/app/(afterLogin)/[username]/status/[id]/_lib/getComments.ts
+++ src/app/(afterLogin)/[username]/status/[id]/_lib/getComments.ts
@@ -13,7 +13,6 @@ export const getComments = async ({
   nextCursor?: number;
   message: string;
 }> => {
-  console.log(id);
   const isServer = typeof window === 'undefined';
   const requestUrl = `${
     isServer ? process.env.SERVER_URL : process.env.NEXT_PUBLIC_SERVER_URL
