34c751ec28c1a061e82c1b165ea9937906d75267
seodevv <seodev17@gmail.com>
2025-10-14 01:22:46 +0900

    modify search list error page

--- src/app/(afterLogin)/i/lists/search/error.tsx
+++ src/app/(afterLogin)/i/lists/search/error.tsx
@@ -1,6 +1,9 @@
 'use client';
 
-import ListsError from '@/app/(afterLogin)/i/lists/[listid]/error';
+import styles from './error.module.css';
+import cx from 'classnames';
+import RefreshSvg from '@/app/_svg/error/RefreshSvg';
+import Link from 'next/link';
 
 interface Props {
   error: Error & { digest?: string };
@@ -8,5 +11,29 @@ interface Props {
 }
 
 export default function Error({ error, reset }: Props) {
-  return <ListsError error={error} reset={reset} />;
+  let message = 'Something went wrong. Try reloading.';
+  let button = (
+    <button className={styles.reset} onClick={() => reset()}>
+      <RefreshSvg white inherit />
+      <span className={styles.text}>Retry</span>
+    </button>
+  );
+
+  if (error.message === 'not-found') {
+    message = 'These lists do not exist. Try searching other lists.';
+    button = (
+      <Link className={cx(styles.reset, styles.search)} href="/i/lists/search">
+        Search
+      </Link>
+    );
+  }
+
+  return (
+    <main className={styles.error}>
+      <div className={styles.message}>
+        <span>{message}</span>
+      </div>
+      {button}
+    </main>
+  );
 }
